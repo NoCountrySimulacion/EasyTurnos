85925c0cc72b4ef74793f9ecc5ab6d35b8222521
Juan <Jinbyos@Gmail.com>
2024-05-31 18:53:15 -0300

    chore: add base Appointment repository and interface

---
+++ Backend/Infrastructure/Repositories/AppointmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class AppointmentRepository : GenericRepository<Appointment, Guid>, IAppointmentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AppointmentRepository(IMapper mapper, ApplicationDbContext context) : base(context)
+        {
+            _mapper = mapper;
+        }
+    }
+}
