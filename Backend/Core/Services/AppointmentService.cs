ae77f1d2aaf6a433ec8d7e9563aadf6dbbd2d1d5
Juan <Jinbyos@Gmail.com>
2024-05-31 19:02:32 -0300

    chore: add base Appointment service and interface

---
+++ Backend/Core/Services/AppointmentService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Core.Services.Interfaces;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly IAppointmentRepository _appointmentRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<AppointmentService> _logger;
+
+        public AppointmentService(
+            IAppointmentRepository appointmentRepository,
+            IMapper mapper,
+            ILogger<AppointmentService> logger)
+        {
+            _appointmentRepository = appointmentRepository;
+            _mapper = mapper;
+            _logger = logger;
+
+        }
+    }
+}
