c85825458c2f5d43ea0edb89ead8d10a3de19a73
Olivera Leonardo <olezdev@gmail.com>
2024-06-02 19:43:36 -0300

    feat(dto): add ClientDTO for user data transfer

---
+++ Backend/DTOs/Client/ClientAddDto.cs
@@ -0,0 +1,29 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DTOs.Client;
+
+public class ClientAddDto
+{
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+
+    [Phone]
+    [MinLength(9, ErrorMessage = "Phone number must be at least 9 digits long.")]
+    [MaxLength(15, ErrorMessage = "Phone number cannot exceed 15 digits.")]
+    public string? PhoneNumber { get; set; }
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+
+    //public UserTypeOtions UserType { get; set; } = UserTypeOtions.Client;
+}
