84f67e1253bea25feeb4bcbc31688824d0583934
Olivera Leonardo <olezdev@gmail.com>
2024-06-04 14:26:22 -0300

    feat: add properties for related ApplicationUser data

--- Backend/DTOs/Client/ClientGetDto.cs
+++ Backend/DTOs/Client/ClientGetDto.cs
@@ -6,4 +6,8 @@ public class ClientGetDto
 {
     public Guid Id { get; set; }
     public DateTime BirthDate { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 }
