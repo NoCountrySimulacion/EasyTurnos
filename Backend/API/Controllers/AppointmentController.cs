9708e569aaddb2870e56774fd0a69fed5a8d4f94
Juan <Jinbyos@Gmail.com>
2024-06-05 17:42:41 -0300

    chore: change endpoints paths

--- Backend/API/Controllers/AppointmentController.cs
+++ Backend/API/Controllers/AppointmentController.cs
@@ -4,7 +4,6 @@ using DTOs.Slot;
 using DTOs;
 using Microsoft.AspNetCore.Mvc;
 using DTOs.Appointment;
-using System.ComponentModel.DataAnnotations;
 
 namespace API.Controllers
 {
@@ -19,13 +18,13 @@ namespace API.Controllers
             _appointmentService = appointmentService;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<AppointmentGetDto>>>> AddAppointment([Required]Guid clientId, [Required]Guid professionalId, AppointmentAddDto newAppointment)
+        [HttpPost("{clientId}/{professionalId}")]
+        public async Task<ActionResult<ServiceResponse<List<AppointmentGetDto>>>> AddAppointment(Guid clientId, Guid professionalId, AppointmentAddDto newAppointment)
         {
             return Ok(await _appointmentService.AddAppointment(clientId, professionalId, newAppointment));
         }
 
-        [HttpDelete]
+        [HttpDelete("{appointmentId}/{professionalId}")]
         public async Task<ActionResult<ServiceResponse<List<AppointmentGetDto>>>> DeleteAppointment(Guid appointmentId, Guid professionalId)
         {
             return Ok(await _appointmentService.DeleteAppointment(appointmentId, professionalId));
