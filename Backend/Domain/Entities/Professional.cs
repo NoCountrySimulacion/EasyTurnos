624e8d9c22f3ef0d3b3da4243368eb37c2a18d96
luisjavierluna <javierlunaluis@gmail.com>
2024-06-06 12:07:38 -0600

    feat: improve update professional method with additional fields

--- Backend/Domain/Entities/Professional.cs
+++ Backend/Domain/Entities/Professional.cs
@@ -5,6 +5,7 @@
         public ApplicationUser ApplicationUser { get; set; }
         public string Speciality { get; set; } = "No specialty established";
         public string Description { get; set; } = "No Description established";
+        public string Location { get; set; } = "No Location established";
         public ICollection<ProfessionalClient> ProfessionalClients { get; set; }
         public ICollection<Record> Records { get; set; }
         public ICollection<Slot> Slots { get; set; }
