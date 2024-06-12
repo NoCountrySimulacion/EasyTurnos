7475e2f12540bfc48ccb228a38627d67c724b9fb
TechSergio <smarquez15@gmail.com>
2024-06-12 15:49:24 -0300

    eliminar cliente

--- Frontend/src/services/api/clientServices.ts
+++ Frontend/src/services/api/clientServices.ts
@@ -43,3 +43,33 @@ export async function updateClient(
 		throw error
 	}
 }
+
+
+
+export const deleteClient = async (
+	token: string | undefined,
+	professionalId: string | undefined,
+	clientId: string
+) => {
+	try {
+		const response = await fetch(
+			`${BASE_UPDATE_CLIENT}${professionalId}/clients/${clientId}`,
+			{
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`
+				}
+			}
+		)
+
+		if (!response.ok) {
+			const errorData = await response.json()
+			throw new Error(errorData.message || 'Error al eliminar el cliente')
+		}
+
+		return true // Retorna verdadero si la solicitud fue exitosa
+	} catch (error) {
+		console.error('Error al actualizar los datos del cliente:', error)
+		throw error	}
+}
