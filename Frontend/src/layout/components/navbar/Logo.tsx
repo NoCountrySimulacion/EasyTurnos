a3aa4814b594a0890d000e354bdcc0b4ab6d8ed4
Facundo Pettersson <petterssonfacundo@gmail.com>
2024-06-11 22:05:36 -0300

    chore: AddLogo

--- Frontend/src/layout/components/navbar/Logo.tsx
+++ Frontend/src/layout/components/navbar/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo(): JSX.Element {
 	return (
 		<div className=' w-44 text-orange-400 text-lg font-medium font-roboto leading-relaxed'>
 			<NavLink to={'/'}>
-				Easy Turnos
+				<img src='../../../../public/svgs/Logo_2.svg' alt='' />
 			</NavLink>
 		</div>
 	)
